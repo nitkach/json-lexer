4677264dec9222949aaef5a4b8d6d6ec62c5badc
Nikita <nikita_tkachev_2001@mail.ru>
2023-12-24 02:20:01 +0300

    move mare-website to separate repo

--- mare-website/migrations/20231123184331_create-mares-table.up.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists mares (
-             id serial       primary key,
-           name varchar(100) not null,
-          breed integer      not null,
-    modified_at timestamptz  not null     default (now()::timestamp)
-);
